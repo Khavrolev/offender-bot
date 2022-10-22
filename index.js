86d730904510d8897eb0048e0aeb7a18b5340405
Sergey Khavrolev <sergey.khavrolev@coolbet.com>
2022-10-22 23:12:02 +0300

    add bot and logger

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+const axios = require("axios");
+const { logger } = require("./logger");
+require("dotenv").config();
 const TelegramApi = require("node-telegram-bot-api");
 
-require("dotenv").config();
-
+const url = "https://evilinsult.com/generate_insult.php";
 const bot = new TelegramApi(process.env.TOKEN, { polling: true });
 
-bot.on("message", (message) => {
-  console.log(message);
-});
+const start = async () => {
+  bot.setMyCommands([{ command: "/offend", description: "Оскорби" }]);
+
+  bot.on("message", async (message) => {
+    const text = message.text;
+    const chatId = message.chat.id;
+    const username = message.from.username;
+
+    try {
+      if (text === "/start") {
+        return bot.sendMessage(chatId, `Здарова, @${username}, заебал!`);
+      }
+
+      if (text === "/offend") {
+        const { data } = await axios.get(url, { params: { lang: "ru" } });
+        return bot.sendMessage(chatId, data.replace(/\s\s+/g, " "));
+      }
+
+      return bot.sendMessage(
+        chatId,
+        `@${username}, ты что за хуету тут понаписал, пидор!`
+      );
+    } catch (error) {
+      logger.info(error);
+      return bot.sendMessage(chatId, "Что-то какая-то хуета произошла, пидор");
+    }
+  });
+};
+
+start();
